c14613711cf1b64aca2baad8cfc4af6bf92d9b49
Ollie-Judge <ollie.judge@hotmail.com>
2023-06-06 13:19:05 +0100

    added an overlay to grey out the background

--- script.js
+++ script.js
@@ -7,10 +7,12 @@ console.log(showModalButton);
 
 const openModal = function () {
   modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
 };
 
 const closeModal = function () {
   modal.classList.add("hidden");
+  overlay.classList.add("hidden");
 };
 
 for (let i = 0; i < showModalButton.length; i++)
